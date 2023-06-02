57f9cef8073282160074d01587c17f78ec563e3b
rei00c <reicountzero@gmail.com>
2023-06-02 15:39:06 +0900

    alert表示テスト

--- js/theendwin10.js
+++ js/theendwin10.js
@@ -3,4 +3,4 @@ const endOfSupport = new Date('2025-10-14');
 const timeDiff = endOfSupport.getTime() - today.getTime();
 const daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
 
-console.log(`Windows 10のサポート終了まであと${daysLeft}日です。`);
+alert(`Windows 10のサポート終了まであと${daysLeft}日です。`);
