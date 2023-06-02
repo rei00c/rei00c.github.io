7974ec043cc53d229c979afb3b32ec8279836b40
rei00c <reicountzero@gmail.com>
2023-06-02 16:08:10 +0900

    スクリプトエラー修正

--- js/theendwin10.js
+++ js/theendwin10.js
@@ -3,4 +3,4 @@ const endOfSupport = new Date('2025-10-14');
 const timeDiff = endOfSupport.getTime() - today.getTime();
 const daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
 
-alert(`Windows 10のサポート終了まであと${daysLeft}日です。`);
+// alert(`Windows 10のサポート終了まであと${daysLeft}日です。`);
