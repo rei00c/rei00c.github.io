84c25c366dd9376a4a4614c15a163e7d07a7bb5f
rei00c <reicountzero@gmail.com>
2023-06-15 10:36:31 +0900

    Javascript 日時表示の変更

--- js/dateandtime.js
+++ js/dateandtime.js
@@ -1,3 +1,12 @@
 setInterval(function () {
-  document.getElementById("d2").innerHTML = new Date().toLocaleString();
+  var now = new Date();
+  var year = now.getFullYear();
+  var month = ('0' + (now.getMonth() + 1)).slice(-2);
+  var day = ('0' + now.getDate()).slice(-2);
+  var hours = ('0' + now.getHours()).slice(-2);
+  var minutes = ('0' + now.getMinutes()).slice(-2);
+  var seconds = ('0' + now.getSeconds()).slice(-2);
+
+  var formattedDate = year + '/' + month + '/' + day + ' ' + hours + minutes + seconds;
+  document.getElementById("d2").innerHTML = formattedDate;
 }, 1000);
