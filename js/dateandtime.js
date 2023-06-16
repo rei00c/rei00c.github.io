fb74de0c006ec9fe5a5abaf3bdfc06199235d8e9
rei00c <reicountzero@gmail.com>
2023-06-16 11:04:06 +0900

    Javascriptの簡潔化

--- js/dateandtime.js
+++ js/dateandtime.js
@@ -1,12 +1,5 @@
 setInterval(function () {
-  var now = new Date();
-  var year = now.getFullYear();
-  var month = ('0' + (now.getMonth() + 1)).slice(-2);
-  var day = ('0' + now.getDate()).slice(-2);
-  var hours = ('0' + now.getHours()).slice(-2);
-  var minutes = ('0' + now.getMinutes()).slice(-2);
-  var seconds = ('0' + now.getSeconds()).slice(-2);
-
-  var formattedDate = year + '/' + month + '/' + day + ' ' + hours + minutes + seconds;
+  const now = new Date();
+  const formattedDate = `${now.getFullYear()}/${(now.getMonth() + 1).toString().padStart(2, '0')}/${now.getDate().toString().padStart(2, '0')} ${now.getHours().toString().padStart(2, '0')}${now.getMinutes().toString().padStart(2, '0')}${now.getSeconds().toString().padStart(2, '0')}`;
   document.getElementById("d2").innerHTML = formattedDate;
 }, 1000);
