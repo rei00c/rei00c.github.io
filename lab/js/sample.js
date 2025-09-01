30c32a6b775fa15d159c1c0fbde299ace32b8a37
rei00c <reicountzero@gmail.com>
2025-09-01 19:32:57 +0900

    JavaScriptサンプルを更新し、価格計算機能を削除して偶数・奇数判定機能を追加

--- lab/js/sample.js
+++ lab/js/sample.js
@@ -1,9 +1,8 @@
-// 金額
-var price = 19800;
-// 税率
-var tax = 0.08;
-// 計算
-var result = price * (1.0 + tax);
-// 結果
-var message = price + "円の税込価格は" + result + "円。";
-document.write('<p>' + message + '</p>');
+// 調べる数字
+var num = 12345;
+// 2で割って余りがあるかどうか
+if (num % 2 == 0) {
+    document.write('<p>' + num + 'は偶数です。</p>');
+} else {
+    document.write('<p>' + num + 'は奇数です。</p>');
+}
