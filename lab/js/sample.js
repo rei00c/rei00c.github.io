6b16baa91a1847dfcea34595fa8ea256ce859ece
rei00c <reicountzero@gmail.com>
2025-09-01 19:24:03 +0900

    JavaScriptサンプルに価格計算機能を追加

--- lab/js/sample.js
+++ lab/js/sample.js
@@ -1,1 +1,9 @@
-document.write("<p>This is JavaScript Sample!</p>");
+// 金額
+var price = 19800;
+// 税率
+var tax = 0.08;
+// 計算
+var result = price * (1.0 + tax);
+// 結果
+var message = price + "円の税込価格は" + result + "円。";
+document.write('<p>' + message + '</p>');
