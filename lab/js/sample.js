4c9a98eca61b6c985b2d975cc9785f80a2f2c99d
rei00c <reicountzero@gmail.com>
2025-09-02 14:56:18 +0900

    JavaScriptサンプルを更新し、じゃんけんゲームを合計計算機能に置き換え

--- lab/js/sample.js
+++ lab/js/sample.js
@@ -1,17 +1,13 @@
-// 人間の手
-var janken = "グー";
-document.write('<p>あなたは' + janken + '</p>');
-// 人間の手をチェック
-switch (janken) {
-    case "グー":
-        document.write('<p>私は、パー。</p>');
-        break;
-    case "チョキ":
-        document.write('<p>私は、グー。</p>');
-        break;
-    case "パー":
-        document.write('<p>私は、チョキ。</p>');
-        break;
-    default:
-        document.write('<p>……なんだって？？</p>');
+// 最大値
+var max = 100;
+// 現在の値
+var num = 1;
+// 合計を加算していくための変数 
+var total = 0;
+// 最大値まで繰り返す
+for (var num = 1; num <= max; num++) {
+    // 合計に現在の値を加算
+    total += num;
 }
+// 合計を表示
+document.write('<p>' + max + 'までの合計は、' + total + 'です。</p>');
