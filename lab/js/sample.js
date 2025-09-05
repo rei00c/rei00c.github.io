7e76c7c477d902565634c89cf7325528911ca46d
rei00c <reicountzero@gmail.com>
2025-09-05 16:07:23 +0900

    サンプルHTMLファイルに入力フィールドとボタンを追加し、JavaScriptでテキスト入力に応じたメッセージ表示機能を実装

--- lab/js/sample.js
+++ lab/js/sample.js
@@ -1,15 +1,9 @@
-// 配列を用意する
-var data = new Array();
-// 値を収めていく
-data['name'] = '山田太郎';
-data['mail'] = 'taro@yamada';
-data['tel'] = '090-1234-5678';
-// 関数を作成する
-data.print = function() {
-    var result = '<p>' + this.name + '</p>';
-    result += '<p>' + this.mail + '</p>';
-    result += '<p>' + this.tel+ '</p>';
-    document.write(result);
+function clickBtn() {
+  // id='txt'のDOMを得る
+  var txt = document.getElementById('txt1');
+  // valueを使ってメッセージを作成
+  var result ='こんにちは、' + txt.value + 'さん。';
+  // id='msg'のDOMを得る
+  var msg = document.getElementById('msg');
+  msg.textContent = result;
 }
-// 処理を実行する
-data.print(data);
