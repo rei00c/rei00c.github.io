3bd8d113eb19ad717144573c7cdda57fce083092
rei00c <reicountzero@gmail.com>
2025-09-11 16:16:55 +0900

    サンプルHTMLにリストを追加し、ボタンクリック時に入力内容をリストに追加する機能を実装

--- lab/js/sample.js
+++ lab/js/sample.js
@@ -1,10 +1,10 @@
 function clickBtn() {
-  // id='txt'のDOMを得る
-  var txt = document.getElementById('txt1');
-  // valueを使ってメッセージを作成
-  var result ='この色は、 ' + txt.value + ' です。';
-  // id='msg'のDOMを得る
   var msg = document.getElementById('msg');
-  msg.textContent = result;
-  msg.style.color = txt.value;
+  var txt = document.getElementById('txt1');
+  var list = document.getElementById('list');
+  var tag = document.createElement('li');
+  tag.textContent = txt.value;
+  list.appendChild(tag);
+  msg.textContent = '「' + txt.value + '」を追加しました。';
+  text.value = '';
 }
