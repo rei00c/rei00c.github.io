f93501fbbddbb80f062fbedb82e5256ef9529571
rei00c <reicountzero@gmail.com>
2025-09-11 15:58:32 +0900

    JavaScriptサンプルを更新し、メッセージ表示内容を変更し、入力色に応じたテキスト色を設定する機能を追加

--- lab/js/sample.js
+++ lab/js/sample.js
@@ -2,8 +2,9 @@ function clickBtn() {
   // id='txt'のDOMを得る
   var txt = document.getElementById('txt1');
   // valueを使ってメッセージを作成
-  var result ='こんにちは、' + txt.value + 'さん。';
+  var result ='この色は、 ' + txt.value + ' です。';
   // id='msg'のDOMを得る
   var msg = document.getElementById('msg');
   msg.textContent = result;
+  msg.style.color = txt.value;
 }
