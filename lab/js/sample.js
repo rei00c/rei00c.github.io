186a849335c9a4399ebb7cb8e148485700837971
rei00c <reicountzero@gmail.com>
2025-09-12 14:36:59 +0900

    サンプルHTMLにonload属性を追加し、JavaScriptでメッセージを表示する初期化関数を実装。ボタンクリック時に入力内容をローカルストレージに保存する機能を追加。

--- lab/js/sample.js
+++ lab/js/sample.js
@@ -1,10 +1,24 @@
-function clickBtn() {
-  var msg = document.getElementById('msg');
+function init() {
+  // id='txt'のDOMを得る
   var txt = document.getElementById('txt1');
-  var list = document.getElementById('list');
-  var tag = document.createElement('li');
-  tag.textContent = txt.value;
-  list.appendChild(tag);
-  msg.textContent = '「' + txt.value + '」を追加しました。';
-  text.value = '';
+  // 保存した値を取り出す
+  var str = localStorage.getItem('msg');
+  // id='msg'のDOMを得る
+  var msg = document.getElementById('msg');
+  // テキストをDOMのコンテンツに設定
+  msg.textContent = 'メッセージ:「' + str + '」';
+}
+
+function clickBtn() {
+  // id='txt'のDOMを得る 
+  var txt = document.getElementById('txt1');
+  localStorage.setItem('msg', txt.value);
+  // valueを使ってメッセージを作成
+  var result = 'メッセージ:「' + txt.value + '」を保存しました。';
+  // id='msg'のDOMを得る
+  var msg = document.getElementById('msg');
+  // テキストをDOMのコンテンツに設定
+  msg.textContent = result;
+  // 入力フィールドを空にする
+  txt.value = '';
 }
