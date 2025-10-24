6676bcde0f64370741133475e97ffcd61b27dc97
rei00c <reicountzero@gmail.com>
2025-10-24 11:26:58 +0900

    Add support for Extended Security Updates (ESU) in Windows 10 page, including new countdown display

--- lab/js/theendwin10.js
+++ lab/js/theendwin10.js
@@ -4,3 +4,8 @@ const timeDiff = endOfSupport.getTime() - today.getTime();
 const daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
 
 // alert(`Windows 10のサポート終了まであと${daysLeft}日です。`);
+
+// ESU追加
+const endOfSupportESU = new Date('2026-10-13');
+const timeDiffESU = endOfSupportESU.getTime() - today.getTime();
+const daysLeftESU = Math.ceil(timeDiffESU / (1000 * 3600 * 24));
